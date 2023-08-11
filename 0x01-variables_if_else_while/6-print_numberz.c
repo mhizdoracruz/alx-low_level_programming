7f8bceea3646e5085f3400b664a1d79bcbcc49b4
Dominic chiyenkem Dorathy <dorathydominic3@gmail.com>
2023-08-11 16:47:38 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Return: Return 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
