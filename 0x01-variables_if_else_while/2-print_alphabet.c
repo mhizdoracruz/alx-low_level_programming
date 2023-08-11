6cda656af874630bff44df8ba3110db16b6cb9e7
Dominic chiyenkem Dorathy <dorathydominic3@gmail.com>
2023-08-11 15:55:48 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - The main function
+ * Return: return 0
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
