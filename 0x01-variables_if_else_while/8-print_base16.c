1db19cc19dbe7c18cb340ca28fdd8b4ddc4e4c42
Dominic chiyenkem Dorathy <dorathydominic3@gmail.com>
2023-08-11 17:00:53 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Return: Return 0
+ */
+
+int main(void)
+{
+	char i;
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		putchar(c + '0');
+	}
+	for (i = 'a'; i < 'g'; i++)
+	putchar('\n');
+	return (0);
+}
