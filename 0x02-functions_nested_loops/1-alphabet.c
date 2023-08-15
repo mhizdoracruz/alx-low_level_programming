e9ce4bce741783a26473c216c0991a2c1fc1726b
Dominic chiyenkem Dorathy <dorathydominic3@gmail.com>
2023-08-15 15:26:49 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,15 @@
  * Return: this will return 0
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char ch;
-
+	
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		_putchar(ch);
 	}
+	
 	_putchar('\n');
-	return (0);
+	return;
 }
