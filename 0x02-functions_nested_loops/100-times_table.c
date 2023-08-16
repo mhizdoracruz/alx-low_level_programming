db6d64a98c3b8fd97d738e857534e6d98f743590
Dominic chiyenkem Dorathy <dorathydominic3@gmail.com>
2023-08-16 14:42:42 -0700

    Write a function that prints the n times table, starting with 0.
    Prototype: void print_times_table(int n);

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_times_table - This the time table function
+ *
+ * @n: the n parameter
+ *
+ * Return: this will return void
+ */
+
+void print_times_table(int n)
+{
+	if (n > 15 || n < 0)
+	{
+		return;
+	}
+}
