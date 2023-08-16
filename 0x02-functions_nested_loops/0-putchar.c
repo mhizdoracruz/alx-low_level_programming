6da1099d252cdd99a396496dea5f6aac36c11cf9
Dominic chiyenkem Dorathy <dorathydominic3@gmail.com>
2023-08-16 04:18:07 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,9 +8,14 @@
 
 int main(void)
 {
-	char ch = "_putchar";
-	
-	_putchar(ch);
+	char c[9] = "_putchar";
+	int i = 0;
+
+	 while (i < 8)
+	 {
+		 _putchar(c[i]);
+		 i++;
+	 }
 	_putchar('\n');
 	return (0);
 }
