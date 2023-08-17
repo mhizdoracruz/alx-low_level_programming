53463bae4eadd05bce4a0460d017384caa0b88a3
Dominic chiyenkem Dorathy <dorathydominic3@gmail.com>
2023-08-17 02:30:25 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,8 +10,44 @@
 
 void print_times_table(int n)
 {
+	int a = 0, result, b;
+
 	if (n > 15 || n < 0)
 	{
 		return;
+	}
+	while (a <= n)
+	{
+		for (b = 0; b <= n; b++)
+		{
+			result = a * b;
+			if (b == 0)
+				_putchar('0' + result);
+			else if (result < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + result);
+			}
+			else if (result < 100)
+			{
+				_putchar(' ');
+				_putchar('0' + (result / 10));
+				_putchar('0' + (result % 10));
+			}
+			else
+			{
+				_putchar('0' + (result / 100));
+				_putchar('0' + (result - 100) / 10);
+				_putchar('0' + (result % 10));
+			}
+			if (b < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+		a++;
 	}
 }
