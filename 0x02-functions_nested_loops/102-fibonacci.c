294d057e15dbea8bb0e50a4d67185415c9b14d71
Dominic chiyenkem Dorathy <dorathydominic3@gmail.com>
2023-08-17 02:54:19 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio>
+
+/**
+ * main - The main function
+ *
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 1; c <=50 c++)
+	{
+		putchar('0' + c);
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
