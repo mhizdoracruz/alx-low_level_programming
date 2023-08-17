d1fe6b35ecaaf30a9600c08259f0f1147e7f92b5
Dominic chiyenkem Dorathy <dorathydominic3@gmail.com>
2023-08-17 15:02:24 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - The print number function
+ *
+ * Return: This will return void
+ */
+
+void more_numbers(void)
+{
+	int c, i;
+
+	c = 0;
+	while (c < 10)
+	{
+		i = 0;
+		while (i < 15)
+		{
+			if (i > 9)
+				_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			i++;
+		}
+		c++;
+		_putchar('\n');
+	}
+}
