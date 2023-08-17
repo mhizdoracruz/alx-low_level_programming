37091431115d4d63ce37a841d16ba152297aee1d
Dominic chiyenkem Dorathy <dorathydominic3@gmail.com>
2023-08-17 14:08:50 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - The print number function
+ *
+ * Return: This will return void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
