b514c77159ed0f17075808dcae7535ee884fab72
Dominic chiyenkem Dorathy <dorathydominic3@gmail.com>
2023-08-19 06:30:41 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_number - The print number function
+ *
+ * @n: the n parameter
+ *
+ * Return: This will return void
+ */
+
+void print_number(int n)
+{
+	unsigned int n1 = 0;
+
+	if  (n < 0)
+	{
+		n1 = -n;
+		_putchar('-');
+	}
+	else
+	{
+		n1 = n;
+	}
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
+}
