c78228dff8a40c6580aa7ecb40d3a89922245983
Dominic chiyenkem Dorathy <dorathydominic3@gmail.com>
2023-08-19 04:07:24 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - The print square function
+ *
+ * @size: The size of the square
+ *
+ * Return: The will returnn void
+ */
+
+void print_square(int size)
+{
+	int x, y;
+
+	y = 0;
+	if (size < 1)
+		_putchar('\n');
+	while (y < size)
+	{
+		x = 0;
+		while (x < size)
+		{
+			_putchar('#');
+			x++;
+		}
+		_putchar('\n');
+		y++;
+	}
+}
