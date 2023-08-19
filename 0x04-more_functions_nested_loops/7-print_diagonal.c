979ffa91c5ebc47719606dd3241646cfd2438296
Dominic chiyenkem Dorathy <dorathydominic3@gmail.com>
2023-08-19 03:44:35 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,13 +10,22 @@
 
 void print_diagonal(int n)
 {
-	int c;
+	int c, i;
 
 	c = 0;
-	while (c < n)
+	while (n > 0)
 	{
+		i = c;
+		while (i > 0)
+		{
+			_putchar(' ');
+			i--;
+		}
 		_putchar('\\');
+		_putchar('\n');
 		c++;
+		n--;
 	}
-	_putchar('\n');
+	if (c < 1)
+		_putchar('\n');
 }
