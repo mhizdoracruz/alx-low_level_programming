d94620599018ede996c2e9b04a9460d80ef94cee
Dominic chiyenkem Dorathy <dorathydominic3@gmail.com>
2023-08-24 13:47:33 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdbool.h>
+#include <ctype.h>
+#include <stdio.h>
+
+/**
+ * cap_string - Check code
+ *
+ * @str: Pointer
+ *
+ * Return: the string str
+ */
+
+bool is_separator(char c)
+{
+	char separators[] = "\t\n,;.!?\"(){}";
+	int i;
+
+	for (i = 0; separators[i] != '\0'; i++)
+	{
+		if (c == separators[i])
+		{
+			return (true);
+		}
+	}
+	return (false);
+}
+
+char *cap_string(char *str)
+{
+	bool new_word = true;
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if (is_separator(*ptr))
+		{
+			new_word = true;
+		}
+		else if (new_word && islower(*ptr))
+		{
+			*ptr = toupper(*ptr);
+			new_word = false;
+		}
+		else if (!new_word && isupper(*ptr))
+		{
+			*ptr = tolower(*ptr);
+		}
+		else
+		{
+			new_word = false;
+		}
+		ptr++;
+	}
+	return (str);
+}
