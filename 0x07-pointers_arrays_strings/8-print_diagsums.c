a344b1ec276b4d71a0f02521b23d0127dde6c361
Dominic chiyenkem Dorathy <dorathydominic3@gmail.com>
2023-08-28 17:42:13 -0700

    prinnt-diagonal

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - check the code
+ *
+ * @a: pointer
+ * @size: paramter
+ *
+ * Return: nothing
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int sum1 = 0;
+	int sum2 = 0;
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i * size + i];
+		sum2 += a[i * size + (size - 1 - i)];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
