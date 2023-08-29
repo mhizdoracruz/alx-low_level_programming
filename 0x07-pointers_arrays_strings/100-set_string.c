46520d26a682debf266a199fa642269f2b5d3cc4
Dominic chiyenkem Dorathy <dorathydominic3@gmail.com>
2023-08-28 17:50:53 -0700

    set-string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_string - Check the code
+ *
+ * @s: pointer
+ * @to: pointer
+ *
+ * Return: nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
