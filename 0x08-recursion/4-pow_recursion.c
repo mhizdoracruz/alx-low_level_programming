762dfdc0953019190bda0652820c6f113f6d6a7b
Dominic chiyenkem Dorathy <dorathydominic3@gmail.com>
2023-08-30 14:41:57 -0700

    Recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - the exponentiate function
+ *
+ * @x: parameter
+ * @y: parameter
+ *
+ * Return: Return the value
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
