9dbef56dea97e13c20ba5f239ed24c604443bef4
Dominic chiyenkem Dorathy <dorathydominic3@gmail.com>
2023-09-02 12:36:17 -0700

    recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - The function to print the natural square of a number
+ *
+ * @n: parameter
+ * @guess: parameter
+ *
+ * Return: to return -1 when number does not have a natural square root
+ */
+
+int _sqrt_recursive(int n, int guess)
+{
+	if (guess * guess == n)
+	{
+		return (guess);
+	}
+	else if (guess * guess > n)
+	{
+		return (-1);
+	}
+	else
+	{
+		return _sqrt_recursive(n, guess + 1);
+	}
+}
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return _sqrt_recursive(n, 1);
+	}
+}
