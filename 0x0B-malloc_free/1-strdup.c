2591f412884b3ab6c8d05d38bbf6acd2c9de2b6c
Dominic chiyenkem Dorathy <dorathydominic3@gmail.com>
2023-09-06 01:39:39 -0700

    String Duplicate copy

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - Check the code
+ *
+ * @str: Pointer
+ *
+ * Return: Return NULL if insufficient memory was available
+ */
+
+char *_strdup(char *str)
+{
+	char *duplicate = (char *)malloc(strlen(str) + 1);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	if (duplicate == NULL)
+        {
+                return (NULL);
+        }
+	strcpy (duplicate, str);
+	return (duplicate);
+}
