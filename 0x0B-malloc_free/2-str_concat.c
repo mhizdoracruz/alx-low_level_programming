dbf6d81788132a080a5b6b26a7c3f18df8245adc
Dominic chiyenkem Dorathy <dorathydominic3@gmail.com>
2023-09-07 00:20:49 -0700

    concatenate strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,12 +19,12 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		s1 = "";
+		return (NULL);
 	}
 	if (s2 == NULL)
         {
-                s2 = "";
-        }
+		return (NULL);
+	}
 	if (result == NULL)
 	{
 		return (NULL);
