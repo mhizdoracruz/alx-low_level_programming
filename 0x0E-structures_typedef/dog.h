d57d0c3f07ef38875a882dd045e2b4c2ecffc412
Dominic chiyenkem Dorathy <dorathydominic3@gmail.com>
2023-09-12 12:27:30 -0700

    Dog File

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+  * struct dog - Dog attributes
+  * @name: The name of the dog.
+  * @age: The age of the dog.
+  * @owner: The owner of the dog.
+  *
+  * Description: The attributes of a dog.
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* MAIN_H */
